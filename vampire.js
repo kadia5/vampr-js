c08c76e28a13f091c587be19ccbbe580d0a88941
kadia5 <kdabraeux@gmail.com>
2021-08-09 17:03:53 +0000

    got totalDascendants

--- vampire.js
+++ vampire.js
@@ -38,8 +38,6 @@ class Vampire {
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
-   
     //if this.yearConverted of 1 vamp is > another vampire 
     //if theyre a creator vampire
     // if (vampire.creator > vampire.offspring || vampire.creator) {
@@ -56,17 +54,89 @@ class Vampire {
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
+  //how to access root vamp
+//   findFirstVamp() {
+//     let currentVamp = this;
+//     if (currentVamp.creator) {
+//       currentVamp = currentVamp.creator;
+//       //recursive line is below
+//       return currentVamp.findFirstVamp()
+//   } else {
+//     //stop condition below
+//     return currentVamp;
+//   }
+  
+// }
+
   vampireWithName(name) {
-    
+      // console.log(this); 
+      function findName(vampire, name){
+        // console.log(vampire)
+        if (vampire && vampire.name === name){
+          return vampire;
+        }
+        if(vampire.offspring.length === 0){
+          // console.log("here we go")
+          return null;
+        
+        }
+        //child = the entire object within the offspring arr, called vampire/all its info
+        for (let child of vampire.offspring) {
+          // console.log("this is child");
+          // console.log(child)
+          // console.log(name)
+          //recursion line 89
+          let childVamp = findName(child, name);
+          // console.log("this is a string")
+          // console.log(childVamp)
+          if (childVamp) {
+            return childVamp
+          } else {
+            continue;
+          }
+        
+        }
+        return null;
+      }
+     
+     let result = findName(this, name);
+     console.log(result)
+     return result
+      
   }
+  
 
   // Returns the total number of vampires that exist
+  //when theres a get funct, no need to put ()
   get totalDescendents() {
+    let descendents = 0;
     
+    //counts for current vamp + offsprings in for loop
+    //loops only hen vamp has offsprings, else wont loop
+      for (let child of this.offspring) {
+        // recursion is line below, calling method again on 1st descendant counted
+        //the counts theirs
+        descendents += 1 + child.totalDescendents
+         
+       
+    }
+    return descendents; 
   }
+
 
   // Returns an array of all the vampires that were converted after 1980
   get allMillennialVampires() {
+    let allMillennialVampires = []; 
+
+    if (this.yearConverted > 1980) {
+      allMillennialVampires.push(this); 
+    }
+
+    for (const child of this.offspring) {
+      allMillennialVampires.concat(child.allMillennialVampires) 
+    }
+
+    return allMillennialVampires;
     
   }
 
@@ -77,9 +147,9 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+//   closestCommonAncestor(vampire) {
 
-  }
+//   }
 }
 
 
